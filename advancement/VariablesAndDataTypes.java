09dcc2efae618c016a73ff708a5967030c7c42e6
Ryan Kulyassa <73004474+rkulyassa@users.noreply.github.com>
2021-05-05 20:40:43 -0400

    Create VariablesAndDataTypes.java

---
+++ advancement/VariablesAndDataTypes.java
@@ -0,0 +1,20 @@
+class VariablesAndDataTypes {
+  public static void main(String[] args) {
+    /*
+    Other data types not shown here include
+    byte, short, long, and double
+    */
+
+    boolean bool = true;
+    int number = 5;
+    float otherNumber = -6.5f;
+    char letter = 'a';
+    String myName = "Ryan";
+    
+    System.out.println("boolean with value: " + bool);
+    System.out.println("int with value: " + number);
+    System.out.println("float with value: " + otherNumber);
+    System.out.println("char with value: " + letter);
+    System.out.println("String with value: " + myName);
+  }
+}
