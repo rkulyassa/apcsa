f0eea607041b64d5bebf2edad4e1a2422a3e4563
Ryan Kulyassa <73004474+rkulyassa@users.noreply.github.com>
2021-05-05 20:40:18 -0400

    Create PrintStatements.java

---
+++ advancement/PrintStatements.java
@@ -0,0 +1,7 @@
+class PrintStatements {
+  public static void main(String[] args) {
+    System.out.println("This is output with a new line.");
+    System.out.print("This is output without a new line.");
+    System.out.printf("This is %s output.", "formatted");
+  }
+}
